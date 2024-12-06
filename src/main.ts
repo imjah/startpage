ab15be1e6a0668e7a42760db593768cdff774830
imjah <imjah@tuta.io>
2024-12-06 13:13:37 +0100

    Move modern-normalize import

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { mount } from 'svelte'
-import '../node_modules/modern-normalize/modern-normalize.css';
 import './app.css'
 import App from './App.svelte'
 
-const app = mount(App, {
+export default mount(App, {
   target: document.getElementById('app')!,
 })
-
-export default app
