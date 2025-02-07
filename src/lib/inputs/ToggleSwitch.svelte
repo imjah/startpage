ed957b18c0737ca543269f17d4140c17e20faeb8
imjah <imjah@tuta.io>
2025-02-07 20:37:16 +0100

    Add toggle switch

---
+++ src/lib/inputs/ToggleSwitch.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  let { checked = $bindable() } = $props()
+</script>
+
+<div class="toggle-switch">
+  <input class="toggle-switch__input" type="checkbox" bind:checked>
+  <div class="toggle-switch__slider"></div>
+</div>
+
+<style lang="scss">
+  .toggle-switch {
+    $width: 60px;
+    $height: 34px;
+    $offset: 4px;
+
+    position: relative;
+    width: $width;
+    height: $height;
+
+    &__input { 
+      width: 0;
+      height: 0;
+      opacity: 0;
+
+      &:checked + .toggle-switch__slider::before {
+        background: var(--color-accent);
+        transform: translateX($width - $height);
+      }
+    }
+
+    &__slider {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: var(--color-surface);
+      cursor: pointer;
+
+      &::before {
+        content: "";
+        position: absolute;
+        width: $height - 2 * $offset;
+        height: $height - 2 * $offset;
+        left: $offset;
+        bottom: $offset;
+        background: var(--color-surface-light);
+        transition: transform .1618s;
+      }
+    }
+  }
+</style>
