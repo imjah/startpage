6236352be8a3ce0d578656e953061bc6e9989df2
imjah <imjah@tuta.io>
2025-05-18 13:02:26 +0200

    Add wheel ability to focus navigator

--- src/util/focus.svelte.ts
+++ src/util/focus.svelte.ts
@@ -3,19 +3,19 @@ export class FocusNavigator {
   items: HTMLElement[] = $state([]);
 
   at(index: number) {
-    this.index = index
-    this.current()
-  }
+    this.index = index;
+    this.current();
+  };
 
   current() {
     this.items[this.index].focus();
   };
 
   prev() {
-    this.at(Math.max(0, this.index - 1));
+    this.at(this.index - 1 < 0 ? this.items.length - 1 : this.index - 1);
   };
 
   next() {
-    this.at(Math.min(this.items.length - 1, this.index + 1));
+    this.at((this.index + 1) % this.items.length);
   };
 };
